6fc8e4e35b939e02973ffe731fa4215396982ee1
jbickelhaupt <jbickelhaupt@wsu.edu>
2014-08-08 11:24:55 -0700

    new theme files
    various customization changes (css, php, etc.)

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package Make
+ */
+?>
+
+				</div>
+<center><div class="custom-logo"><a rel="home" href="/"></a></div></center>
+			</div>
+
+<div class="border"></div>
+
+			<?php ttfmake_maybe_show_site_region( 'footer' ); ?>
+		</div>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package Make
+ */
+?><!DOCTYPE html>
+<!--[if lte IE 9]><html class="no-js IE9 IE" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body <?php body_class(); ?>>
+		<div id="site-wrapper" class="site-wrapper">
+
+<?php
+if ( is_front_page() ) { ?>
+<div id="logos">
+<a href="http://www.wsu.edu/"><img id="wsu" src="/wp-content/uploads/wsu.png" /></a>
+<a href="http://www.wwu.edu/"><img id="wwu" src="/wp-content/uploads/wwu.png" /></a>
+<a href="http://www.hiu.edu/"><img id="hiu" src="/wp-content/uploads/hiu.png" /></a>
+<a href="http://www.bothell.washington.edu/"><img id="uw-bothell" src="/wp-content/uploads/uw-bothell.png" /></a>
+<a href="http://www.ewu.edu/"><img id="ewu" src="/wp-content/uploads/ewu.png" /></a>
+<a href="http://www.evergreen.edu/"><img id="esc" src="/wp-content/uploads/esc.png" /></a>
+<a href="http://www.cwu.edu/"><img id="cwu" src="/wp-content/uploads/cwu.png" /></a>
+</div>
+
+<?php } else {
+    // This is for all other pages/posts
+
+}
+?>
+
+<div class="border"></div>
+
+			<?php ttfmake_maybe_show_site_region( 'header' ); ?>
+
